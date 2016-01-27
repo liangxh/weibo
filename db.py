28a0c06d683139ebbf62b8f6be74dfb404caf6e5
liangxh12 <sekhou64>
2016-01-27 21:35:32 +0800

    singleton for db

--- db.py
+++ db.py
@@ -1,6 +1,21 @@
+
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+'''
+Author: Xihao Liang
+Created: 2016.01.20
+Modified: 2016.01.27
+Description: a script for access to mysql.weibo
+'''
+
 import MySQLdb as db
 
+con = None
+
 def connect():
+	if con:
+		return con
+
 	try:
 		con = db.connect('localhost', 'weiboguest', 'weiboguest', 'weibo')
 		return con
