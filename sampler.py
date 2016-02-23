0f452d98fb46d8a527ff647d41e430faaeec323b
liangxh <sekhou64@hotmail.com>
2016-02-23 14:54:45 +0800

    files rearranged

--- sampler.py
+++ sampler.py
@@ -13,7 +13,7 @@ reload(sys)
 sys.setdefaultencoding('utf8')
 
 import cPickle
-from const import DIR_EID_MID_UID
+from const import DIR_EID_UIDS
 
 def muid2umids(mid_uid):
 	'''
@@ -32,10 +32,10 @@ def muid2umids(mid_uid):
 
 def load_mid_uid(eid):
 	'''
-	load {mid:uid, ...} from data/eid_mid_uid/$EID.pkl
+	load {mid:uid, ...} from data/eid_uids/$EID.pkl
 	'''
 
-	fname = DIR_EID_MID_UID + '%d.pkl'%(eid)
+	fname = DIR_EID_UIDS + '%d.pkl'%(eid)
 	
 	if not os.path.exists(fname):
 		print '[Warning] %s not found'%(fname)
@@ -43,38 +43,11 @@ def load_mid_uid(eid):
 
 	return cPickle.load(open(fname, 'r'))
 
-def analyse_mid_uid(eid):
-	mid_uid = load_mid_uid(eid)
+def sample(eid):
+	'''
+	sampling text from MySQL to data/dataset/text/EID.txt and data/dataset/muid/EID.txt
+	'''
 
-	umids = muid2umids(mid_uid)
-	
-	uid_mids = sorted(umids.items(), key = lambda k: -len(k[1]))
-
-	c = 0
-	target = 4000
-
-	for i, item in enumerate(uid_mids):
-		uid, mids = item
-		print '%d. %d'%(i + 1, len(mids))
-
-		n_mids = len(mids)
-		
-		if n_mids > 100:
-			c += 100
-		else:
-			c += n_mids
-		
-		if c >= target:
-			break
-
-	coverage = 100.
-	if c < target:
-		coverage = 100. * c / target
-	
-	print '==================================='
-	print 'coverage: %0.2f%%'%(coverage)
-
-def sample_text(eid):
 	from share import blogger
 	import db
 	con = db.connect()
@@ -117,8 +90,12 @@ def sample_text(eid):
 		texts = texts[:target]
 		str_mid_uid = str_mid_uid[:target]
 
-	open('output/text_%d.txt'%(eid), 'w').write('\n'.join(texts))
-	open('output/muid_%d.txt'%(eid), 'w').write('\n'.join(str_mid_uid))
+	DIR_DATASET = 'data/dataset/'
+	DIR_TEXT = DIR_DATASET + 'text/'
+	DIR_MUIDS = DIR_DATASET + 'muid/'
+
+	open(DIR_TEXT + '%d.txt'%(eid), 'w').write('\n'.join(texts))
+	open(DIR_MUIDS + '%d.txt'%(eid), 'w').write('\n'.join(str_mid_uid))
 
 if __name__ == '__main__':	
 	import sys
