97d77a98bbb74c539642e84cb7ea12d5955d92b5
liangxh <sekhou64@hotmail.com>
2016-02-27 11:00:55 +0800

    hntest updated

--- hntest.py
+++ hntest.py
@@ -9,6 +9,7 @@ Description: script used to test the coverage the hownet
 import sys
 reload(sys)
 sys.setdefaultencoding('utf8')
+import cPickle
 
 import tokenprocessor as tokenpr
 from hownet import HowNet
@@ -23,28 +24,26 @@ def test(n_emo = N_EMO):
 	content_supported = 0	
 
 	token_all = set()
+	
 	token_supported = {}
 	
 	pbar = progbar.start(n_emo * 4000)
 
 	i = 0
-
 	token_not_supported = {}
 	cover_dist = []
 
-
 	eseqs = tokenpr.load()
-	for eid, seqs in enumerate(eseqs):
+	for eid, seqs in enumerate(eseqs[:n_emo]):
 		for seq in seqs:
 			content_all += len(seq)
 			
 			sup_len = 0
 			for token in seq:
-				token_all.add(token)
-				
 				if hownet.support(token):
 					sup_len += 1
 					content_supported += 1
+
 					if token_supported.has_key(token):
 						token_supported[token] += 1
 					else:
@@ -57,30 +56,42 @@ def test(n_emo = N_EMO):
 
 			cover_dist.append((len(seq), sup_len))
 
-	
-
-	'''
-	for eid in range(n_emo):
-		lines =  open(DIR_TEXT + '%d.txt'%(eid), 'r').read().split('\n')
-		for line in lines:
-			tokens = zhprocessor.tokenize(line)
-			n_content += len(tokens)
-			for token in tokens:
-				token_all.add(token)
-				if hownet.support(token):
-					content_coverage += 1
-					token_supported.add(token)
 			i += 1
 			pbar.update(i)
-	'''
 
 	pbar.finish()
 
-	print 'n_token: %d'%(len(token_all))
-	print 'token_coverage: %.1f%%'%(100. * len(token_supported) / len(token_all))
-	print 'n_content: %d'%(n_content)
-	print 'content_coverage: %.1f%%'%(100. * content_coverage / n_content)
+	n_sup_token = len(token_supported.keys())
+	n_nsup_token = len(token_not_supported.keys())
+	n_token = n_sup_token + n_nsup_token
 
+	print 'n_token: %d'%(n_token)
+	print 'token_coverage: %.1f%%'%(100. * n_sup_token / n_token)
+	
+	print 'n_content: %d'%(content_all)
+	print 'content_coverage: %.1f%%'%(100. * content_supported / content_all)
+
+	def save1(fname, tfhist):
+		fobj = open(fname, 'w')
+		for i, item in enumerate(sorted(tfhist.items, key = lambda k:-k)):
+			t, tf = item
+			fobj.write('%d. %s (%d)\n'%(i + 1, t, tf))
+		fobj.close()
+
+
+	save1('output/token_supported.txt', token_supported)
+	save1('output/token_not_supported.txt', token_not_supported)
+
+	cPickle.dump(cover_dist, open('output/coverdist.pkl', 'w'))
+	
+	plt.figure()
+	x = [float(b) / a  for a, b in cover_dist]
+	n, bins, patches = plt.hist(x, 50, normed = 1, alpha=0.75)
+	
+	plt.title('Distribution of HowNet Coverage')
+	plt.xlabel('Coverage')
+	plt.xlabel('Number of Blogs')
+	plt.show()
 
 if __name__ == '__main__':
 	test(1)
